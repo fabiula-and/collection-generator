4430d13e05583ca59579a57dc480b67d55f8eb2e
fabiula-and <142566453+fabiula-and@users.noreply.github.com>
2026-08-26 00:54:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ const temas = [
     "Hospital"
 ];
 
+let modoSelecionado = "ideia";
+
+document.getElementById("modo-ideia").addEventListener("click", function() {
+    modoSelecionado = "ideia";
+    console.log("Modo escolhido:", modoSelecionado);
+});
+
+document.getElementById("modo-estampa").addEventListener("click", function() {
+    modoSelecionado = "estampa";
+    console.log("Modo escolhido:", modoSelecionado);
+});
+
+document.getElementById("modo-mini").addEventListener("click", function() {
+    modoSelecionado = "mini";
+    console.log("Modo escolhido:", modoSelecionado);
+});
+
+document.getElementById("modo-completa").addEventListener("click", function() {
+    modoSelecionado = "completa";
+    console.log("Modo escolhido:", modoSelecionado);
+});
+
+document.getElementById("modo-caos").addEventListener("click", function() {
+    modoSelecionado = "caos";
+    console.log("Modo escolhido:", modoSelecionado);
+});
+
 document.getElementById("gerar").addEventListener("click", function() {
 
     const temaSorteado = temas[Math.floor(Math.random() * temas.length)];
